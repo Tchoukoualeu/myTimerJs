0f36b312df7d3a5d23006860fdb3a272c10bd09a
Aris Tchoukoualeu <38705757+Tchoukoualeu@users.noreply.github.com>
2019-07-23 16:19:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+let now = 0;
+let duration = 0;
+let r;
+let backup = 0;
+let toggle = true;
+
+function oneCount(){
+  duration = Date.now() - now;
+  document.getElementById('output').innerText = timeFormatter(duration);
+}
+
+// Starts the time
+function start(){ 
+  // console.log(duration)
+  if(toggle){
+    now = Date.now() - backup;
+    r = setInterval(oneCount, 10)
+    toggle = false; //this helps limit the number of setInterval started
+  }
+}
+
+// Stops the timer
+function stop(){
+  backup = 0;
+  clearInterval(r)
+  toggle = true; 
+  backup = duration; //This helps start where we stopped
+}
+
+// Resets the time
+function reset(){
+  document.getElementById('output').innerText = '00 : 00 . 000';
+  backup = 0; //Make sure this is zero
+}
+
+// Records lapses
+function lapses(){
+  lapsesOut(document.getElementById('output').innerText)
+}
+
+// Helps render lapses
+function lapsesOut(element) {
+  var ul = document.getElementById("lapOutput");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(element));
+  ul.appendChild(li);
+}
+
+// Clear the lapses recorded
+function clean(){
+  document.getElementById('lapOutput').innerText = ''
+}
+
+// helps format the time
+function timeFormatter(time) {
+  time = new Date(time);
+
+  var minutes = time.getMinutes().toString();
+  var seconds = time.getSeconds().toString();
+  var milliseconds = time.getMilliseconds().toString();
+
+  if (minutes.length < 2) {
+    minutes = "0" + minutes;
+  }
+
+  if (seconds.length < 2) {
+    seconds = "0" + seconds;
+  }
+
+  while (milliseconds.length < 3) {
+    milliseconds = "0" + milliseconds;
+  }
+  return minutes + " : " + seconds + " . " + milliseconds;
+}

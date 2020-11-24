9048f79ad0c7ee9d539d9e447c8e0edda42e563b
Aris Tchoukoualeu <38705757+Tchoukoualeu@users.noreply.github.com>
2020-11-24 20:33:02 +0100

    revert

--- script.js
+++ script.js
@@ -1,96 +1,86 @@
-class theTimer {
-  constructor(now, duration, backup, toggle) {
-    this.now = now,
-    this.duration = duration,
-    this.r = null,
-    this.backup = backup,
-    this.toggle = toggle,
-    this.oneCount = this.oneCount();
-    this.timeFormatter = this.timeFormatter();
-  }
+let now = 0;
+let duration = 0;
+let r;
+let backup = 0;
+let toggle = true;
 
-  oneCount() {
-    this.duration = Date.now() - this.now;
-    document.getElementById('output').innerText = this.timeFormatter(this.duration);
-  }
-
-  // Starts the time
-  start() {
-    // console.log(duration)
-    if (this.toggle) {
-      this.now = Date.now() - this.backup;
-      this.r = setInterval(this.oneCount, 10)
-      this.toggle = false; //this helps limit the number of setInterval started
-    }
-  }
-
-  // Stops the timer
-  stop() {
-    this.backup = 0;
-    clearInterval(this.r)
-    this.toggle = true;
-    this.backup = this.duration; //This helps start where we stopped
-  }
-
-  // Resets the time
-  reset() {
-    document.getElementById('output').innerText = '00 : 00 . 000';
-    this.backup = 0; //Make sure this is zero
-    if (!this.toggle) {
-      notificationBox();
-    }
-  }
-
-  // Records lapses
-  lapses() {
-    lapsesOut(document.getElementById('output').innerText)
-  }
-
-  // Rrender lapses
-  lapsesOut(element) {
-    var ul = document.getElementById("lapOutput");
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(element));
-    ul.appendChild(li);
-  }
-
-  // Clear the lapses recorded
-  clean() {
-    document.getElementById('lapOutput').innerText = ''
-  }
-
-  // helps format the time
-  timeFormatter(time) {
-    time = new Date(time);
-
-    var minutes = time.getMinutes().toString();
-    var seconds = time.getSeconds().toString();
-    var milliseconds = time.getMilliseconds().toString();
-
-    if (minutes.length < 2) {
-      minutes = "0" + minutes;
-    }
-
-    if (seconds.length < 2) {
-      seconds = "0" + seconds;
-    }
-
-    while (milliseconds.length < 3) {
-      milliseconds = "0" + milliseconds;
-    }
-    return minutes + " : " + seconds + " . " + milliseconds;
-  }
-
+function oneCount(){
+  duration = Date.now() - now;
+  document.getElementById('output').innerText = timeFormatter(duration);
 }
 
-let clock = new theTimer(0, 0, 0, true);
+// Starts the time
+function start(){ 
+  // console.log(duration)
+  if(toggle){
+    now = Date.now() - backup;
+    r = setInterval(oneCount, 10)
+    toggle = false; //this helps limit the number of setInterval started
+  }
+}
 
+// Stops the timer
+function stop(){
+  backup = 0;
+  clearInterval(r)
+  toggle = true; 
+  backup = duration; //This helps start where we stopped
+}
+
+// Resets the time
+function reset(){
+  document.getElementById('output').innerText = '00 : 00 . 000';
+  backup = 0; //Make sure this is zero
+  if(!toggle){
+    notificationBox();
+  }
+}
+
+// Records lapses
+function lapses(){
+  lapsesOut(document.getElementById('output').innerText)
+}
+
+// Helps render lapses
+function lapsesOut(element) {
+  var ul = document.getElementById("lapOutput");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(element));
+  ul.appendChild(li);
+}
+
+// Clear the lapses recorded
+function clean(){
+  document.getElementById('lapOutput').innerText = ''
+}
+
+// helps format the time
+function timeFormatter(time) {
+  time = new Date(time);
+
+  var minutes = time.getMinutes().toString();
+  var seconds = time.getSeconds().toString();
+  var milliseconds = time.getMilliseconds().toString();
+
+  if (minutes.length < 2) {
+    minutes = "0" + minutes;
+  }
+
+  if (seconds.length < 2) {
+    seconds = "0" + seconds;
+  }
+
+  while (milliseconds.length < 3) {
+    milliseconds = "0" + milliseconds;
+  }
+  return minutes + " : " + seconds + " . " + milliseconds;
+}
 
 // Notification box message
 let box = document.getElementById('notification-box')
-function notificationBox() {
+function notificationBox(){
   box.style.display = 'block';
-  setTimeout(function () {
-    box.style.display = 'none';
+  setTimeout(function(){ 
+    box.style.display = 'none'; 
   }, 3000);
 }
